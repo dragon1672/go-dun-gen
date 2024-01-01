c645a3e8bb3c6d751c5e7fc2d62d2da195b17ba4
Anthony Corbin <dragon1672@gmail.com>
2024-01-01 01:14:21 -0500

    Modifying auto play to prefer exploring deeper

--- driver.go
+++ driver.go
@@ -5,14 +5,22 @@ import (
 
 	"github.com/dragon1672/go-dun-gen/dungeon"
 	"golang.org/x/exp/rand"
+	"golang.org/x/exp/slices"
 )
 
 func AutoPlay(seed string, limit int) {
 	d := dungeon.EnterTheDungeon(seed)
 	fmt.Println(d.Description())
+	var lastMove *dungeon.Direction
 	for i := 0; i < limit; i++ {
 		possibleMoves := d.ValidMoves()
+		if lastMove != nil && len(possibleMoves) > 1 {
+			slices.DeleteFunc(possibleMoves, func(d dungeon.Direction) bool {
+				return d == lastMove.Reverse()
+			})
+		}
 		move := possibleMoves[rand.Intn(len(possibleMoves))]
+		lastMove = &move
 		if err := d.Move(move); err != nil {
 			fmt.Printf("Encountered error: %v", err)
 			return
